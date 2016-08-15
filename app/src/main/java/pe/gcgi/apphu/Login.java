f1755c098c00aaf4ef7aab895603b68e37270161
jsus85 <jsus.guevara@gmail.com>
2016-08-15 01:57:01 -0500

    Registro y Login conexion a los webservice

--- app/src/main/java/pe/gcgi/apphu/Login.java
+++ app/src/main/java/pe/gcgi/apphu/Login.java
@@ -1,30 +1,115 @@
 package pe.gcgi.apphu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity implements View.OnClickListener  {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private RequestQueue requestQueue;
+    private Context context;
+
+    private EditText password;
+    private EditText email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findViewById(R.id.button_send_login).setOnClickListener(this);
+
+        context = Login.this;
+        requestQueue = Volley.newRequestQueue(context);
+
+        //input controls
+        password = (EditText) findViewById(R.id.password);
+        email    = (EditText) findViewById(R.id.email);
     }
 
     @Override
     public void onClick(View view) {
-        final Intent intent = new Intent(Login.this,ConfigureAccount.class);
+
 
         switch (view.getId()){
             case R.id.button_send_login:
-                startActivity(intent);
+
+                Register_User register_user  = new Register_User();
+
+                if(password.length()<3){
+                    password.setError("Error en Password.");
+                    password.requestFocus();
+                }else if(email.length()<3){
+                    email.setError("Error en Email.");
+                    email.requestFocus();
+                }else if(!register_user.validateEmail(email.getText().toString())){
+                    email.setError("Error en Email invalido");
+                    email.requestFocus();
+                }else{
+
+                    login();
+                }
+
                 break;
 
         }
+
+    }
+
+    public void login(){
+
+        String url = "http://gcgi.pe/apphudemo/CCLogin.php?usermail="+email.getText().toString()+"&password="+password.getText().toString();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null, new
+                Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+
+                            if(response.getString("status").equalsIgnoreCase("1") ){
+                                JSONObject jsonObject = new JSONObject(response.toString());
+                                JSONArray jsonArray = jsonObject.getJSONArray("info");
+
+                                Toast.makeText(Login.this, "Bienvenido "+jsonArray.getJSONObject(0).getString("cli_nom"), Toast.LENGTH_SHORT).show();
+                                final Intent intent = new Intent(Login.this,ConfigureAccount.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(Login.this, "Error en Email y Clave incorrecto", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(getClass().getName(),error.getMessage());
+            }
+        }
+
+        );
+        requestQueue.add(jsonObjectRequest);
 
     }
 }
--- app/src/main/java/pe/gcgi/apphu/Register_User.java
+++ app/src/main/java/pe/gcgi/apphu/Register_User.java
@@ -1,26 +1,50 @@
 package pe.gcgi.apphu;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.MediaCodec;
+
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Register_User extends AppCompatActivity implements View.OnClickListener {
 
-     EditText names;
+    EditText names;
     EditText password;
     EditText email;
+
+    private String url = "http://gcgi.pe/apphudemo/CCAccount.php";
+    private StringRequest stringRequest;
+    private Context context;
+    private RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        context = Register_User.this;
+        requestQueue = Volley.newRequestQueue(context);
+
         findViewById(R.id.button_redirect_login).setOnClickListener(this);
         findViewById(R.id.button_register_user).setOnClickListener(this);
 
@@ -32,6 +56,7 @@ public class Register_User extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
+
         final Intent intent = new Intent(Register_User.this, Login.class);
         switch (view.getId()){
             case R.id.button_redirect_login:
@@ -52,18 +77,68 @@ public class Register_User extends AppCompatActivity implements View.OnClickList
                     email.setError("Error en Email invalido");
                     email.requestFocus();
                 }else{
-
-                    startActivity(intent);
-
+                    registerUser();
                 }
 
                 break;
         }
     }
 
+    public void registerUser(){
+
+        stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //Log.d("rta_servidor", response);
+                        if(response.equalsIgnoreCase("succes") ){
+                            final Intent intent = new Intent(Register_User.this, Login.class);
+                            startActivity(intent);
+                        }else{
+                            //Toast.makeText(context, response, Toast.LENGTH_SHORT).show();
+                            // Error Email registrado
+                            new AlertDialog.Builder(context)
+                                    .setTitle("Lo Sentimos")
+                                    .setMessage("Email registrado , desea recuperar la clave? ")
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface arg0, int arg1) {
+                                            // Some stuff to do when ok got clicked
+                                        }
+                                    })
+                                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface arg0, int arg1) {
+                                            // Some stuff to do when cancel got clicked
+                                        }
+                                    })
+                                    .show();
+                        }
+
+
+                    }
+                },  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error_servidor", error.toString());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams()  {
+                Map<String, String> parametros = new HashMap<>();
+
+                parametros.put("username", names.getText().toString());
+                parametros.put("password", password.getText().toString());
+                parametros.put("usermail", email.getText().toString());
+
+                return parametros;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+    }
 
     //Return true if email is valid and false if email is invalid
-    protected boolean validateEmail(String email) {
+    public boolean validateEmail(String email) {
         String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                 + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
@@ -72,5 +147,6 @@ public class Register_User extends AppCompatActivity implements View.OnClickList
 
         return matcher.matches();
     }
+
 
 }
